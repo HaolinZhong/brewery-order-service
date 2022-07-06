54c670741debf082fe807350b489b8103cb2e161
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-06 17:22:14 -0400

    init project

---
+++ src/test/java/hz/spring/breweryorderservice/BreweryOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hz.spring.breweryorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
