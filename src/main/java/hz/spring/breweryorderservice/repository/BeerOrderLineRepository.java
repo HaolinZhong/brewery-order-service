28237a167c689c44d7aaecec385eaa1980080c33
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-06 18:12:59 -0400

    add data repositories

---
+++ src/main/java/hz/spring/breweryorderservice/repository/BeerOrderLineRepository.java
@@ -0,0 +1,9 @@
+package hz.spring.breweryorderservice.repository;
+
+import hz.spring.breweryorderservice.domain.BeerOrderLine;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerOrderLineRepository extends PagingAndSortingRepository<BeerOrderLine, UUID> {
+}
