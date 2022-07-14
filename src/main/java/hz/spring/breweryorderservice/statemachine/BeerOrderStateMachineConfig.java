1e380fbf3016ee4cda270712584780a892d7513d
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-13 22:53:10 -0400

    add annotation for statemachine config class

--- src/main/java/hz/spring/breweryorderservice/statemachine/BeerOrderStateMachineConfig.java
+++ src/main/java/hz/spring/breweryorderservice/statemachine/BeerOrderStateMachineConfig.java
@@ -2,11 +2,15 @@ package hz.spring.breweryorderservice.statemachine;
 
 import hz.spring.breweryorderservice.domain.BeerOrderEventEnum;
 import hz.spring.breweryorderservice.domain.BeerOrderStatusEnum;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 
 import java.util.EnumSet;
 
+@Configuration
+@EnableStateMachineFactory
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
 
     @Override
