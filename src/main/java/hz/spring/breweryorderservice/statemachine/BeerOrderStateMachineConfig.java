92853beb9b2321314de82483d5f6c7dfcf5e8056
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-13 22:52:33 -0400

    configure statemachine states

---
+++ src/main/java/hz/spring/breweryorderservice/statemachine/BeerOrderStateMachineConfig.java
@@ -0,0 +1,24 @@
+package hz.spring.breweryorderservice.statemachine;
+
+import hz.spring.breweryorderservice.domain.BeerOrderEventEnum;
+import hz.spring.breweryorderservice.domain.BeerOrderStatusEnum;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+                .end(BeerOrderStatusEnum.PICKED_UP)
+                .end(BeerOrderStatusEnum.DELIVERED)
+                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+
+    }
+}
