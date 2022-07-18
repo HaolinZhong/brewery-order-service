aa050d054438ff1df919c602077bf5ae568ffb84
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-18 14:32:23 -0400

    add test for allocation error or allocation inventory pending

--- src/test/java/hz/spring/breweryorderservice/service/BeerOrderManagerImplIT.java
+++ src/test/java/hz/spring/breweryorderservice/service/BeerOrderManagerImplIT.java
@@ -124,6 +124,42 @@ class BeerOrderManagerImplIT {
     }
 
     @Test
+    void testFailedAllocation() throws JsonProcessingException {
+        BeerDTO beerDTO = BeerDTO.builder().id(beerId).upc("12345").build();
+
+        wireMockServer.stubFor(get(BeerServiceImpl.BEER_UPC_PATH_V1 + "12345")
+                .willReturn(okJson(objectMapper.writeValueAsString(beerDTO))));
+
+        BeerOrder beerOrder = createBeerOrder();
+        beerOrder.setCustomerRef("failed-allocation");
+
+        BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+
+        await().untilAsserted(() -> {
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
+            assertEquals(BeerOrderStatusEnum.ALLOCATION_EXCEPTION, foundOrder.getOrderStatus());
+        });
+    }
+
+    @Test
+    void testPartialAllocation() throws JsonProcessingException {
+        BeerDTO beerDTO = BeerDTO.builder().id(beerId).upc("12345").build();
+
+        wireMockServer.stubFor(get(BeerServiceImpl.BEER_UPC_PATH_V1 + "12345")
+                .willReturn(okJson(objectMapper.writeValueAsString(beerDTO))));
+
+        BeerOrder beerOrder = createBeerOrder();
+        beerOrder.setCustomerRef("partial-allocation");
+
+        BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+
+        await().untilAsserted(() -> {
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
+            assertEquals(BeerOrderStatusEnum.PENDING_INVENTORY, foundOrder.getOrderStatus());
+        });
+    }
+
+    @Test
     void testNewToPickedUp() throws JsonProcessingException {
         BeerDTO beerDTO = BeerDTO.builder().id(beerId).upc("12345").build();
 
--- src/test/java/hz/spring/breweryorderservice/service/testcomponents/BeerOrderAllocationListener.java
+++ src/test/java/hz/spring/breweryorderservice/service/testcomponents/BeerOrderAllocationListener.java
@@ -11,7 +11,6 @@ import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -21,17 +20,38 @@ public class BeerOrderAllocationListener {
 
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
     public void listen(Message msg) {
+
+        boolean hasError = false;
+        boolean isPending = false;
+
         AllocateBeerOrderRequest request = (AllocateBeerOrderRequest) msg.getPayload();
 
+        if (request.getBeerOrderDTO().getCustomerRef() != null &&
+                request.getBeerOrderDTO().getCustomerRef().equals("failed-allocation")) {
+            hasError = true;
+        }
+
+        if (request.getBeerOrderDTO().getCustomerRef() != null &&
+                request.getBeerOrderDTO().getCustomerRef().equals("partial-allocation")) {
+            isPending = true;
+        }
+
+        boolean isPendingFinal = isPending;
+
         request.getBeerOrderDTO().getBeerOrderLines().forEach(line -> {
-            line.setQuantityAllocated(line.getOrderQuantity());
+            if (isPendingFinal) {
+                line.setQuantityAllocated(line.getOrderQuantity() - 1);
+            } else {
+                line.setQuantityAllocated(line.getOrderQuantity());
+            }
         });
+
 
         jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
                 AllocateBeerOrderResult.builder()
-                .beerOrderDTO(request.getBeerOrderDTO())
-                .pendingInventory(false)
-                .allocationError(false)
-                .build());
+                        .beerOrderDTO(request.getBeerOrderDTO())
+                        .pendingInventory(isPending)
+                        .allocationError(hasError)
+                        .build());
     }
 }
